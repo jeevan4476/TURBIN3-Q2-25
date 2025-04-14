5b92f5cb1fac349dd5a93c6cdfffa4c9d045d2bc
jeevan4476 <jeevanr2431@gmail.com>
2025-04-14 22:25:43 +0530

    Added AMM

---
+++ amm/programs/amm/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("JAddDtwQn7oytJ4PkvGGaTdTEWBG7usXxYbjZN1q4dRv");
+
+#[program]
+pub mod amm {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
